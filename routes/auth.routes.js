4edbf1b39e41d04bc2900683bb1298152f833291
Pawel Sobkowski <psobkowski98@gmail.com>
2020-10-24 15:00:44 +0200

    feat: add router path to signIn

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const authController = require("../controllers/auth.controller");
 
 router.post("/auth/signUp", authController.signUp);
+router.post("/auth/signIn", authController.signIn);
 
 module.exports = router;
