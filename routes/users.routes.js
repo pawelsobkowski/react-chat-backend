b45d6daf1ff67830477cfa139652f916465c9490
Pawel Sobkowski <psobkowski98@gmail.com>
2020-11-16 19:03:23 +0100

    feat: create endpoints for invitations

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -6,6 +6,9 @@ const usersController = require("../controllers/users.controller");
 router.get("/users", usersController.usersFindAll);
 router.get("/users/:id", usersController.usersFindById);
 router.put("/users/:id", usersController.updateUser);
+router.put("/sendInvitation", usersController.sendInvitation);
+router.put("/acceptInvitation", usersController.acceptInvitation);
+router.put("/denyInvitation", usersController.denyInvitation);
 router.delete("/users/:id", usersController.deleteUser);
 
 module.exports = router;
