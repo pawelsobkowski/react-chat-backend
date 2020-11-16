35440a27e34601a8ece39f3a7fbd0b46b3e577d1
Pawel Sobkowski <psobkowski98@gmail.com>
2020-11-16 19:02:21 +0100

    feat: create new functions to manage invitations

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,3 +1,4 @@
+const { mongoose } = require("../models");
 const User = require("../models/user.model");
 
 exports.usersFindAll = (req, res) => {
@@ -10,18 +11,18 @@ exports.usersFindAll = (req, res) => {
 };
 
 exports.usersFindById = (req, res) => {
-  User.findById(req.params.id, (err, user) => {
-    if (err) {
-      return next(err);
-    }
-    res.send(user);
-  });
+  User.findById(req.params.id)
+    .populate({ path: "friends", select: "_id fullName photoUrl" })
+    .populate({ path: "invitations", select: "_id fullName photoUrl" })
+    .then((user) => {
+      res.send(user);
+    });
 };
 
 exports.updateUser = (req, res) => {
   User.findByIdAndUpdate(req.params.id, { $set: req.body }, (err) => {
     if (err) {
-      return next(err);
+      res.send(err);
     }
     res.send("User updated.");
   });
@@ -30,8 +31,60 @@ exports.updateUser = (req, res) => {
 exports.deleteUser = (req, res) => {
   User.findByIdAndRemove(req.params.id, (err) => {
     if (err) {
-      return next(err);
+      res.send(err);
     }
     res.send("User deleted.");
   });
+};
+
+exports.sendInvitation = (req, res) => {
+  User.findByIdAndUpdate(
+    req.body.id,
+    {
+      $addToSet: { invitations: [mongoose.Types.ObjectId(req.body.userId)] },
+    },
+    (err) => {
+      if (err) {
+        res.send(err);
+      }
+      res.status(200).send("Friend invitation has been sent");
+    }
+  );
+};
+
+exports.acceptInvitation = (req, res) => {
+  User.updateOne(
+    { _id: req.body.userId },
+    {
+      $push: {
+        friends: [mongoose.Types.ObjectId(req.body.id)],
+      },
+      $pull: {
+        invitations: req.body.id,
+      },
+    },
+    (err) => {
+      if (err) {
+        res.send(err);
+      }
+      res.status(200).send("New contact has been added");
+    }
+  );
+};
+
+exports.denyInvitation = (req, res) => {
+  User.updateOne(
+    { _id: req.body.userId },
+    {
+      $pull: {
+        invitations: req.body.id,
+      },
+    },
+    (err) => {
+      if (err) {
+        res.send(err);
+      }
+      res.status(200).send("Invitation has been deleted");
+    }
+  );
 };
