86ef89ff2b42d96a32b750151ca4fe5ae2451669
Pawel Sobkowski <psobkowski98@gmail.com>
2020-11-26 23:29:27 +0100

    feat: add routes for searchUsers and cancelInvitations

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -5,10 +5,12 @@ const usersController = require("../controllers/users.controller");
 
 router.get("/users", usersController.usersFindAll);
 router.get("/users/:id", usersController.usersFindById);
+router.post("/searchUsers", usersController.usersFindByName);
 router.put("/users/:id", usersController.updateUser);
 router.put("/sendInvitation", usersController.sendInvitation);
 router.put("/acceptInvitation", usersController.acceptInvitation);
 router.put("/denyInvitation", usersController.denyInvitation);
+router.put("/cancelInvitation", usersController.cancelInvitation);
 router.delete("/users/:id", usersController.deleteUser);
 
 module.exports = router;
