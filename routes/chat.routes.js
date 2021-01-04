9edeff303ad0c7149c9a8dfd53f2102442536065
Pawel Sobkowski <psobkowski98@gmail.com>
2021-01-04 17:04:26 +0100

    feat: add routes

--- routes/chat.routes.js
+++ routes/chat.routes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const chatController = require("../controllers/chat.controller");
 
 router.get("/chat", chatController.chatFindAll);
-router.get("/chat/:id", chatController.chatUserFindAll);
+router.get("/chat/:id", chatController.chatFindById);
+router.post("/chat/:id", chatController.sendMessage);
+router.get("/chat/user/:id", chatController.chatUserFindAll);
 
 module.exports = router;
