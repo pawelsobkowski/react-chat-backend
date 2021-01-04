f9e7213eec64db52177c320ec5806d7dcaebfa47
Pawel Sobkowski <psobkowski98@gmail.com>
2021-01-04 17:03:52 +0100

    feat: add functions to find chats and save messages

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -9,6 +9,17 @@ exports.chatFindAll = (req, res) => {
   });
 };
 
+exports.chatFindById = (req, res) => {
+  Chat.find({ _id: req.params.id })
+    .populate({ path: "participants", select: "_id fullName photoUrl" })
+    .then((err, chats) => {
+      if (err) {
+        res.send(err);
+      }
+      res.send(chats);
+    });
+};
+
 exports.chatUserFindAll = (req, res) => {
   Chat.find({ participants: req.params.id })
     .populate({ path: "participants", select: "_id fullName photoUrl" })
@@ -18,4 +29,23 @@ exports.chatUserFindAll = (req, res) => {
       }
       res.send(chats);
     });
+};
+
+exports.sendMessage = (req, res) => {
+  const message = {
+    content: req.body.content,
+    userId: req.body.userId,
+    timestamp: new Date(),
+  };
+
+  Chat.updateOne(
+    { _id: req.params.id },
+    { $addToSet: { messages: [message] } },
+    (err) => {
+      if (err) {
+        res.send(err);
+      }
+      res.status(200).send("Message sent.");
+    }
+  );
 };
